7c6a01ace8dd7e0e872a9f01976c35b26e277c41
Cristian Dávila <krif07@gmail.com>
2023-06-13 11:41:57 -0500

    Purchase service, repository, controller

---
+++ src/main/java/com/code/market/domain/service/PurchaseService.java
@@ -0,0 +1,26 @@
+package com.code.market.domain.service;
+
+import com.code.market.domain.Purchase;
+import com.code.market.domain.repository.PurchaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PurchaseService {
+
+    @Autowired
+    private PurchaseRepository purchaseRepository;
+
+    public List<Purchase> getAll(){
+        return purchaseRepository.getAll();
+    }
+    public Optional<List<Purchase>> getByClient(String clientId){
+        return purchaseRepository.getByClient(clientId);
+    }
+    public Purchase save(Purchase purchase){
+        return purchaseRepository.save(purchase);
+    }
+}
